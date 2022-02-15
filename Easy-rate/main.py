7299055ea02ea192919b337ce1ba6d0f81a611a9
CesarGuzman <47696662+CesarGuzmanLopez@users.noreply.github.com>
2022-02-15 14:43:30 -0600

    Maqueta-1

--- Easy-rate/main.py
+++ Easy-rate/main.py
@@ -34,8 +34,6 @@ class EntradaDato(ttk.Frame) :
         self.botonverfile['state'] ="disabled"
         self.labelEtiquetafilename = ttk.Label(self,text="")
         self.labelEtiquetafilename.grid(row = 1, column = 3,columnspan = 2,padx=4)
-        self.mensajeEsperar:WaitAlert
-        self.EstructuraSeleccionada:Estructura
         self.comando=command
     def view(self):
         ViewStructure(master=self, estructure =self.EstructuraSeleccionada)
@@ -107,15 +105,13 @@ class EasyRate:
         style = ThemedStyle(self.master )
         self.Principal.pack_propagate(True)
         self.Principal.place(anchor='nw', bordermode='outside', x=str(0), y=str(0))
-       
         self.master.title("Marcus 1.1")
         self.master.resizable(False, False)
         self.master.geometry("975x600")
-
         self.FramePrincipal = ttk.Frame(self.Principal)
         self.Principal.configure(width='960',height='605')
         self.menu() 
-        self.SeecionTemperatura()
+        self.Seccion_Datos_2()
         self.SeccionDifusion()
         self.SeccionPantalla() 
         self.SeccionLeerArchivos()
@@ -140,11 +136,9 @@ class EasyRate:
         help.add_command(label="About",command=self.About)
         
         self.master.config(menu=menubar)
-    
-
     def SeccionLeerArchivos(self,pos_x=10,pos_y=10):
         seccionLeerArchivos = ttk.Frame(self.Principal)
-        seccionLeerArchivos.configure(width='360',height='355')
+        seccionLeerArchivos.configure(width='360',height='290')
 
         labelData_entry = ttk.Label(self.Principal,text="Data entry", font=('calibri', 9,"bold"))
         labelData_entry.place(x=str(pos_x), y=str(pos_y))
@@ -186,14 +180,12 @@ class EasyRate:
         self.Prduct_1_vert .Activar(Etiqueta="Product-1(vert.)"
                             ,command=self.defPrduct_1_vert )
         
-
-
     def defReact_1       (self,Estruc:Estructura):
         self.Temperatura.delete(0,END)
         self.Temperatura.insert(0,str(Estruc.temp.getValue))
         self.Temperatura['state'] ="disabled"
         self.React_1.setDato(UnDato=Estruc.free_engergy.getValue)
-        
+
     def defReact_2       (self,Estruc:Estructura):
         self.React_2.setDato(UnDato=Estruc.free_engergy.getValue)
     def defPrduct_1_adiab(self,Estruc:Estructura):
@@ -203,17 +195,20 @@ class EasyRate:
     def defPrduct_1_vert (self,Estruc:Estructura):
         self.Prduct_1_vert.setDato(UnDato=Estruc.scf.getValue)
 
-
-
-    def SeecionTemperatura(self,pos_x=30,pos_y=400): 
-        seccionTemperatura= ttk.Frame(self.Principal)
-        seccionTemperatura.configure(width='200',height='50')
-        seccionTemperatura.place(x=str(pos_x),y=str(pos_y+15))
-        labelEtiquetaTemperatura = ttk.Label(seccionTemperatura,text="Temperature(K)" )
-        labelEtiquetaTemperatura.grid(row = 1, column = 1)
-        self.Temperatura = tk.Entry(seccionTemperatura)
-        self.Temperatura.grid(row = 1, column = 2)
+    def Seccion_Datos_2(self,pos_x=30,pos_y=300): 
+        SeccionDatos2= ttk.Frame(self.Principal)
+        SeccionDatos2.configure(width='200',height='50')
+        SeccionDatos2.place(x=str(pos_x),y=str(pos_y+15))
+        labelEtiquetaTemperatura = ttk.Label(SeccionDatos2,text="Temperature(K)" )
+        labelEtiquetaTemperatura.grid(row = 1, column = 0)
+       
+        self.Temperatura = tk.Entry(SeccionDatos2)
+        self.Temperatura.grid(row = 1, column = 1)
         self.Temperatura.insert(0,"298.15")
+        ttk.Label(SeccionDatos2,text="Texto var uno").grid(column=0,row=0)
+        self.Var_uno=ttk.Entry(SeccionDatos2).grid(column=1,row=0)
+        ttk.Label(SeccionDatos2,text="Texto var uno").grid(column=0,row=2)
+        self.Var_2=ttk.Entry(SeccionDatos2).grid(column=1,row=2)
 
     def SeccionDifusion(self,pos_x=30,pos_y=440):
         seccionDifusion= ttk.Frame(self.Principal)
@@ -242,7 +237,6 @@ class EasyRate:
         labelreact1.place(x="30",y="55")
         self.radius_react_2 = tk.Entry(frame2,width=15,state='disabled')
         self.radius_react_2.place(x="95",y="55")
-
         labelreact1 = ttk.Label(frame2,text="Reaction distance (in Angstroms)")
         labelreact1.place(x="30",y="75")
         self.ReactionDistance = tk.Entry(frame2,width=15,state='disabled')
@@ -259,18 +253,29 @@ class EasyRate:
             self.radius_react_2['state'] = 'disabled'
             self.ReactionDistance['state'] ='disabled'
 
-    def SeccionPantalla(self,pos_x=370,pos_y=10):
+    def SeccionPantalla(self,pos_x=370,pos_y=0):
         seccionPantalla= ttk.Frame(self.Principal)
         seccionPantalla.configure(width='600',height='700')
         seccionPantalla.place(x=str(pos_x),y=str(pos_y))
-       
+        self.Var_3=IntVar()
+        self.Var_3.set(0)
+        ttk.Label(seccionPantalla,text="Texto var uno").place(anchor='nw', x='100', y='0')
+        ttk.Label(seccionPantalla,text="yes").place(anchor='nw', x='170', y='0')
+        ttk.Radiobutton(seccionPantalla,value=1,variable=self.Var_3).place(anchor='nw', x='190', y='0')
+        ttk.Label(seccionPantalla,text="No").place(anchor='nw', x='210', y='0')
+        ttk.Radiobutton(seccionPantalla,value=0,variable=self.Var_3).place(anchor='nw', x='230', y='0')
+        self.Var_4=IntVar()
+        self.Var_4.set(0)
+        ttk.Label(seccionPantalla,text="Texto var uno").place(anchor='nw', x='300', y='0')
+        ttk.Label(seccionPantalla,text="yes").place(anchor='nw', x='370', y='0')
+        ttk.Radiobutton(seccionPantalla,value=1,variable=self.Var_4).place(anchor='nw', x='390', y='0')
+        ttk.Label(seccionPantalla,text="No").place(anchor='nw', x='410', y='0')
+        ttk.Radiobutton(seccionPantalla,value=0,variable=self.Var_4).place(anchor='nw', x='430', y='0')
+
+
         boton = ttk.Button(seccionPantalla,text="Data ok, Run", command=self.run_calc)
-        boton.place(x="250",y="10")
-        
+        boton.place(x="250",y="20")
         self.ScrollePantalla(seccionPantalla)
-
-
-
         labelrate = ttk.Label(seccionPantalla)
         labelrate.configure(cursor='arrow', justify='left', relief='raised', text='Rate constant units:\n-For bimolecular(M-1 s-1)\n -For unimolecular reactions(s-1)')
         labelrate.place(anchor='nw', x='0', y='500')
@@ -300,65 +305,7 @@ class EasyRate:
         xsb2.grid(row=2, column=1, columnspan=1,sticky=E+N+S+W)
         
     def run_calc(self):
-        c =self.radius_react_1.get()
-        b =self.radius_react_2.get()
-        a=self.ReactionDistance.get()
-     
-        react1_G =self.React_1       .getTextValue
-        react2_G =self.React_2       .getTextValue
-        prod1_G  =self.Prduct_1_adiab.getTextValue
-        prod2_G  =self.Prduct_2_adiab.getTextValue
-        prod1_Ev =self.Prduct_1_vert .getTextValue
-        prod2_Ev =self.Prduct_2_vert .getTextValue
-        react1_E =react1_G
-        react2_E =react2_G
-       
-        
-        if self.difusion.get() == 1 and (a =='' or b  =='' or  c ==''  ):
-            messagebox.showerror(title="It is not possible to calculate", message="Please enter the missing value(s)")
-            return
-        
-        aEnergy = 627.5095 * (prod1_G + prod2_G - react1_G - react2_G)
-        aEnergy_round = round(aEnergy, 2)
-        vEnergy = 627.5095 * (prod1_Ev + prod2_Ev - react1_E - react2_E)
-        vEnergy_round = round(vEnergy, 2) 
-        lam = (vEnergy - aEnergy)
-        if lam == 0:
-            messagebox.showerror(title="It is not possible to calculate", message="Please check Reacts and products values")
-            return 
-
-        lambda_round  = round(lam, 2)
-        barrier:float = ((lam / 4) * (1 + (aEnergy / lam)) * (1 + (aEnergy / lam)))
-        barrier_round = round(barrier, 2)
-        temp          =  float(self.Temperatura.get())
-        try:
-            rateCte:float = 2.08e10 * temp * math.exp(-1.0*barrier * 1000 / (1.987 * temp))
-        except:
-            messagebox.showerror(   title  = "Math range error.",
-                                    message= "Please check you data.")
-            rateCte:float = nan                        
-        
-        if self.difusion.get() == 1:
-            radMolA   :float  = float(self.radius_react_1.get())
-            radMolB   :float  = float(self.radius_react_2.get())
-            reactDist :float  = float(self.ReactionDistance.get())
-            diffCoefA :float  = (self.kBoltz * temp) / (6 * 3.14159 * self.visc * radMolA)
-            diffCoefB :float  = (self.kBoltz * temp) / (6 * 3.14159 * self.visc * radMolB)
-            diffCoefAB:float  = diffCoefA + diffCoefB
-            kDiff     :float  = 1000 * 4 * 3.14159 * diffCoefAB * reactDist * 6.02e23
-            kCorrDiff :float  = (kDiff * rateCte) / (kDiff + rateCte)    
-        title =self.Title.get()
-        self.salida.delete('1.0', END)
-        self.salida.insert(END,("Pathway:  " + title + "\n") )
-        self.salida.insert(END,("Adiabatic energy (G) of reaction (kcal/mol):  " + str(round(aEnergy_round,2)) + "\n") )
-        self.salida.insert(END,("Vertical energy (E) of reaction (kcal/mol):  " + str(round(vEnergy_round,2)) + "\n") )
-        self.salida.insert(END,("Reorganization energy (kcal/mol):  " + str(round(lambda_round,2)) + "\n") )
-        self.salida.insert(END,("Reaction barrier (kcal/mol):  " + str(round( barrier_round,2 ))+ "\n") )
-        if self.difusion.get() == 0:
-            self.salida.insert(END,("Rate Constant:  " + str(round(  rateCte,2)) ) )
-        elif self.difusion.get() == 1:
-            self.salida.insert(END,("Rate Constant:  " + str(round(kCorrDiff,2)) ))
-        self.salida.insert(END, "\n\n-----------------------------------------------------\n\n\n")
+        pass
     def About(self):
         pass
     
@@ -376,8 +323,7 @@ class EasyRate:
                     "\t\tReact-2(adiab.): "  +str(self.React_2       .getTextValue) + "\n"+
                     "\t\tProduct-1(adiab.): "+str(self.Prduct_1_adiab.getTextValue) + "\n"+
                     "\t\tProduct-2(adiab.): "+str(self.Prduct_2_adiab.getTextValue) + "\n"+
-                    "\t\tProduct-1(vert.): " +str(self.Prduct_1_vert .getTextValue) + "\n"+
-                    "\t\tProduct-2(vert.): " +str(self.Prduct_2_vert .getTextValue) + "\n________________________\n\nOutput:\n"
+                    "\t\tProduct-1(vert.): " +str(self.Prduct_1_vert .getTextValue) + "\n"
                 )
                 file.write(
                     self.salida.get("1.0", END)+"\n")
