794c646942b3d322a35b9fdee2ba1bfc098b951c
CesarGuzman <47696662+CesarGuzmanLopez@users.noreply.github.com>
2022-05-11 20:26:35 -0500

    Todos los cambios

--- Easy-rate/viewStructure.py
+++ Easy-rate/viewStructure.py
@@ -1,14 +1,13 @@
-import tkinter as tk
-from tkinter import INSERT, N,E,W,S, scrolledtext
+from tkinter import INSERT, N,E,W,S, Scrollbar, Tk, Toplevel, scrolledtext
 from read_log_gaussian.Estructura import Estructura
 class ViewStructure:
     def __init__(self, master=None, estructure =Estructura ):
-        self.principal = tk.Tk() if master is None else tk.Toplevel(master)
+        self.principal = Tk() if master is None else Toplevel(master)
         self.principal.title("View")
         self.principal.resizable(False, False)
         self.principal.geometry("400x400")
         salida = scrolledtext.ScrolledText(self.principal,wrap = "none",width=40,height=20)
-        xsb = tk.Scrollbar(self.principal,orient="horizontal", command=salida.xview)        
+        xsb = Scrollbar(self.principal,orient="horizontal", command=salida.xview)        
         salida.grid(row=0,column =0,columnspan=1)        
         salida.focus()
         salida.configure(xscrollcommand=xsb.set)
